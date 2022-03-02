33da8d6bb94f2760e75dd67087301bf14acc5a68
Tiffany <tiffanyv1998@gmail.com>
2022-03-02 10:50:01 -0800

    player moves & reset

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-document.body.querySelector('script').style.display = 'none';
-
+document.body.querySelector("script").style.display = "none";
 
 /////////////////
 /* BOARD SETUP */
@@ -8,53 +7,54 @@ document.body.querySelector('script').style.display = 'none';
 // we need to create an empty 3x3 board; so we're gonna loop thru and create 3 rows, 3 columns. for players choosing the grid, it could be done by table? not sure.
 
 //this isnt being used, delete(?)
-function createEmptyBoard () {
-    const board = [];
-    
-    for (let i = 0; i < 3; i++) {
-        board.push(new Array(3).fill(null));
-    }
-    
-    return board;
+function createEmptyBoard() {
+  return new Array(9).fill(null);
 }
 
- console.log(createEmptyBoard());
+console.log(createEmptyBoard());
 
 ///////////
 /* STATE */
 ///////////
 
 //this is to have replay ability; will allow the intialState to replace the current state
-const initialState = {
+function buildInitialState() {
+  const initialState = {
+    board: createEmptyBoard(),
     p1: "",
     p2: "",
     numMoves: 0,
     winner: null,
-};
+  };
 
-let state = {...initialState};
+  return initialState;
+}
 
-const p1Input = document.getElementById('p1');
-const p2Input = document.getElementById('p2');
+let state = buildInitialState();
 
-p1Input.addEventListener('change', (e) => {
-    const node = e.target;
-    const playerName = node.value;
-    state.p1 = playerName;
+//tracking who our players are
+const p1Input = document.getElementById("p1");
+const p2Input = document.getElementById("p2");
+
+p1Input.addEventListener("change", (e) => {
+  const node = e.target;
+  const playerName = node.value;
+  state.p1 = playerName;
 });
 
-p2Input.addEventListener('change', (e) => {
-    const node = e.target;
-    const playerName = node.value;
-    state.p2 = playerName;
+p2Input.addEventListener("change", (e) => {
+  const node = e.target;
+  const playerName = node.value;
+  state.p2 = playerName;
 });
 
+//putting buttons into variables
 const playBtn = document.getElementById("play");
 
 const playAgainBtn = document.getElementById("restart");
 
-playBtn.addEventListener('click', () => {
-    console.log(state);
+playBtn.addEventListener("click", () => {
+  console.log(state);
 });
 
 /////////////////////////
@@ -64,31 +64,51 @@ playBtn.addEventListener('click', () => {
 // every time we click a square, we'll check numMoves
 // if numMoves % 2 === 0, it's 'X', otherwise, it's 'O'
 
+const DOMBoard = document.getElementById("board");
 
-for (let i = 0; i < 9; i++) {
-    const square = document.querySelectorAll('.cell')[i];
-    square.addEventListener('click', function(){
-        const move = state.numMoves % 2 === 0 ? 'x' : 'o';
+DOMBoard.addEventListener("click", function (event) {
+  const square = event.target;
 
-        const playerName = state.numMoves % 2 === 0 ? state.p1 : state.p2;
+  if (!square.classList.contains("cell")) {
+    return;
+  }
 
-        this.innerText = move
-        
-        if (state.numMoves === 9) {
-            return;
-        }
+  const move = state.numMoves % 2 === 0 ? "x" : "o";
 
-        state.numMoves++;
+  const playerName = state.numMoves % 2 === 0 ? state.p1 : state.p2;
 
-        console.log ({playerName, move})
+  if (square.innerText === "x" || square.innerText === "o") return;
+  if (state.numMoves === 9) return;
 
+  const squareId = square.dataset.id;
+  state.board[squareId] = move;
+  console.log(state);
 
-    })
+  state.numMoves++;
+
+  renderState();
+});
+
+// renderState can use your state.board to "paint" moves into the DOM
+function renderState() {
+  console.log(state);
+  const boardSquares = document.querySelectorAll(".cell");
+
+  for (let i = 0; i < state.board.length; i++) {
+    const currentMove = state.board[i]; // 'x', 'o', or null
+    if (currentMove) {
+      boardSquares[i].innerText = currentMove;
+    } else {
+      boardSquares[i].innerText = +boardSquares[i].dataset.id + 1;
+    }
+  }
 }
 
+// ok, we have the x's & o's done. we need to have a square not change once it already contains a player's move.
+
 //how to reset
-playAgainBtn.addEventListener('click', function(){
-    // state =  {...initialState};
-
-})
-
+playAgainBtn.addEventListener("click", function () {
+  state = buildInitialState();
+  console.log(state);
+  renderState();
+});
